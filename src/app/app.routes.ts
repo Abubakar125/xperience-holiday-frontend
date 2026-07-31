2bb0353d124703f14e1b3cb612a587c513fc6cf0
Abubakar Afzal <abubakarafzal243@gmail.com>
2026-07-31 20:15:07 +0500

    Completed About Us Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   }
 ];
